27b7f79af50749ba1192768b65618a88abd63a5c
Nadav Ivgi <nadav@shesek.info>
2021-03-25 06:48:57 +0200

    Update bwt

--- bwt/install.sh
+++ bwt/install.sh
@@ -9,14 +9,6 @@ mkdir /var/log/bwt && chown nobody /var/log/bwt
 # Needed to send real-time notifications to the bwt daemon (https://github.com/bwt-dev/bwt#real-time-indexing)
 apt-get install -qqy --no-install-recommends netcat-openbsd
 
-mv bin/* /ez/bin/
-
-# FIXME remove
-if [ -f bwt ] && [ "$BWT_ARCH" == "x86_64-linux" ]; then
-  cp bwt /usr/local/bin/
-  exit 0
-fi
-
 # Install bwt
 distname=bwt-$BWT_VERSION-$BWT_ARCH
 wget -qO /tmp/bwt.tar.gz https://github.com/bwt-dev/bwt/releases/download/v$BWT_VERSION/$distname.tar.gz
@@ -24,3 +16,6 @@ echo "$BWT_SHA256  /tmp/bwt.tar.gz" | sha256sum -c -
 
 tar xzf /tmp/bwt.tar.gz -C /tmp
 mv /tmp/$distname/bwt /usr/local/bin
+
+# Install utitilies
+mv bin/* /ez/bin/
