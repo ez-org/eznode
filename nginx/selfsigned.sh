794b5034fbd71c484c3f4b5087cf41ca4e1df44c
Nadav Ivgi <nadav@shesek.info>
2021-04-14 06:42:46 +0300

    nginx: Switch RSA to EC keys
    Refs #14

--- nginx/selfsigned.sh
+++ nginx/selfsigned.sh
@@ -1,16 +1,16 @@
 # -- expected to be `source`ed
 
-: ${SELFSIGNED_KEYSIZE:=2048}
+: ${SELFSIGNED_CURVE:=secp384r1}
 : ${SELFSIGNED_DAYS:=365}
 : ${SELFSIGNED_SUBJECT:=/}
 
 if [ -f /data/nginx-ssl/selfsigned.key ]; then
   debug ssl Using self-signed certificate in /data/nginx-ssl
 else
-  info ssl Generating RSA-$SELFSIGNED_KEYSIZE key and a self-signed certificate to /data/nginx-ssl
+  info ssl Generating EC-$SELFSIGNED_CURVE key and a self-signed certificate to /data/nginx-ssl
   mkdir -p /data/nginx-ssl
   openssl req -x509 -nodes -subj "$SELFSIGNED_SUBJECT" \
-    -days $SELFSIGNED_DAYS -newkey rsa:$SELFSIGNED_KEYSIZE \
+    -days $SELFSIGNED_DAYS -newkey ec:<(openssl ecparam -name $SELFSIGNED_CURVE) \
     -keyout /data/nginx-ssl/selfsigned.key -out /data/nginx-ssl/selfsigned.crt \
     > /dev/null 2>&1 || error nginx Key generation failed
 fi
